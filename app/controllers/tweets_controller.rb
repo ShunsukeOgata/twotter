f457a71be3caa1ee833be4d15acfc2fd4a5eb868
Shunsuke Ogata <hoaoha7@gmail.com>
2015-02-12 23:34:04 +0900

    tweet機能と削除機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,32 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+	  @tweet = Tweet.new(tweet_params)
+	  if @tweet.save
+	    redirect_to tweets_path
+	  else
+	    render 'new'
+    end
+  end
 
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
+  end
+  
+
+  private
+    
+    def tweet_params
+		  params[:tweet].permit(:content)
+	  end
+	
+	
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ActiveRecord::Base
+  validates :content, 
+  presence: { message: "文字を入力してください" },
+  length: { maximum: 140, message: "140字以内で入力して下さい" }
 end

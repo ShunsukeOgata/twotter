0c2fa0752fe03df57f9055c4b89801fab9f58f59
shunsuke_ogata <hoaloha7@gmail.com>
2015-02-18 18:02:06 +0900

    ユーザ名とアカウント名の無記入と重複を禁止にしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
 
   has_many :tweets
 
-  # validates :name, presence: true, uniqueness: true
-  # validates :account, presence: true, uniqueness: true
-  # validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :account, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
